0bd1a16c932e711f1bdd1aeab8f5bde1dff1540c
Joan Nyawira Kori <korijoan171@gmail.com>
2025-04-16 15:46:33 +0300

    Add timer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      // Cleanup function to clear the timeout if the component unmounts
+      return () => clearTimeout(timerId);
+    } else {
+      // When timeRemaining hits 0
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]); // Dependencies array
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
